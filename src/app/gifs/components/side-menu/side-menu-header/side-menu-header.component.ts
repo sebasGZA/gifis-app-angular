95a5904d6553f43b5850d88910e94f0693a7483c
sebasGZA <sebassga34@gmail.com>
2026-06-26 22:22:45 -0500

    feat: angular envs and  alias path

--- src/app/gifs/components/side-menu/side-menu-header/side-menu-header.component.ts
+++ src/app/gifs/components/side-menu/side-menu-header/side-menu-header.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { environment } from '@environments/environment';
 
 @Component({
   selector: 'app-side-menu-header',
   templateUrl: './side-menu-header.component.html',
 })
-export class SideMenuHeaderComponent {}
+export class SideMenuHeaderComponent {
+  envs = environment
+}
