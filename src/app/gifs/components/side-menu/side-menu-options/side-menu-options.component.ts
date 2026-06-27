553ae4b57bd3d924ce746f8e1d42c111f1484c52
sebasGZA <sebassga34@gmail.com>
2026-06-26 22:04:55 -0500

    feat: routerLinks

--- src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -1,7 +1,31 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+interface MenuOption {
+  icon: string;
+  label: string;
+  route: string;
+  subLabel: string;
+}
 
 @Component({
   selector: 'app-side-menu-options',
   templateUrl: './side-menu-options.component.html',
+  imports: [RouterLink, RouterLinkActive],
 })
-export class SideMenuOptionsComponent {}
+export class SideMenuOptionsComponent {
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      route: 'trending',
+      subLabel: 'Popular gifs',
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Search',
+      route: 'search',
+      subLabel: 'Search gifs',
+    },
+  ]
+}
