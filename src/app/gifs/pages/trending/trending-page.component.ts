d47c59b9326ae8cd925671f7e25c1ad40608bb01
sebasGZA <sebassga34@gmail.com>
2026-06-27 23:26:42 -0500

    feat: infinite scroll

--- src/app/gifs/pages/trending/trending-page.component.ts
+++ src/app/gifs/pages/trending/trending-page.component.ts
@@ -1,12 +1,26 @@
-import { Component, inject } from '@angular/core';
-import { ListComponent } from "../../components/list/list.component";
+import { Component, ElementRef, inject, viewChild } from '@angular/core';
 import { GifService } from '../../services/gifs.service';
 
 @Component({
   selector: 'app-trending',
   templateUrl: './trending-page.component.html',
-  imports: [ListComponent],
 })
 export default class TrendingPageComponent {
   gifsService = inject(GifService);
+
+  scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('groupDiv')
+
+  onScroll() {
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if (!scrollDiv) return;
+    const scrollTop = scrollDiv.scrollTop;
+    const clientHeight = scrollDiv.clientHeight;
+    const maxScroll = scrollDiv.scrollWidth
+
+    const isAtBottom = scrollTop + clientHeight + 100 > maxScroll;
+
+    if (isAtBottom) {
+      this.gifsService.loadTrendingGifs()
+    }
+  }
 }
